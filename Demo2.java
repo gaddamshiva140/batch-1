9bfe1a5651dcf2bf851b7061c73a761ed8a83243
GADDAM SHIVA <gaddamshiva140@gmail.com>
2025-01-03 12:03:18 +0530

    Add files via upload

---
+++ Demo2.java
@@ -0,0 +1,13 @@
+public class Demo2 {
+    public static void main(String[] args) {
+        int num1,num2;
+        int a = 10;
+        int b = 20;
+        if (num1 > num2){
+            System.out.println("num 1 is greater ");
+        }
+        else {
+            System.out.println("num2 is greater ");
+        }
+    }
+}
