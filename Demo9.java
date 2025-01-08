4e9ab9398c19d64ddd143433d874a1b375633b0b
GADDAM SHIVA <gaddamshiva140@gmail.com>
2025-01-08 13:02:53 +0530

    Update Demo9.java

--- Demo9.java
+++ Demo9.java
@@ -1,7 +1,5 @@
-package core java;
-
-public class Demo9 {
-    import java.util.Scanner;
+import java.util.Scanner;
+public class Demo9{    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the base of the triangle: ");
